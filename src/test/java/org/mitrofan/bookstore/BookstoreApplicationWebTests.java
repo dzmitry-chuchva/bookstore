a7547763f4e82dbb437fde0adeec2b546f94ba9c
Dzmitry Chuchva <realmitro@gmail.com>
2023-02-12 20:07:22 +0300

    reduce test scope of BookstoreApplicationWebTests to web layer only

--- src/test/java/org/mitrofan/bookstore/BookstoreApplicationWebTests.java
+++ src/test/java/org/mitrofan/bookstore/BookstoreApplicationWebTests.java
@@ -2,20 +2,27 @@ package org.mitrofan.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-@SpringBootTest
-@AutoConfigureWebTestClient
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@WebFluxTest
 class BookstoreApplicationWebTests {
     @Autowired
     WebTestClient client;
 
+    @MockBean
+    BookRepository bookRepository;
+
     @Test
     void testAddBook() {
+        given(bookRepository.save(any(Book.class))).willReturn(Mono.just(Book.builder().build()));
+
         client.post().uri("/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(Book.builder().build()), Book.class)
