95880ec621cc6952b48da3487cdd9a3ead4a3a67
Dzmitry Chuchva <realmitro@gmail.com>
2023-02-26 13:47:06 +0300

    migrate DB unit tests to TestContainers-based database instance

--- src/test/java/org/mitrofan/bookstore/BookRepositoryAgainstContainerTests.java
+++ src/test/java/org/mitrofan/bookstore/BookRepositoryAgainstContainerTests.java
@@ -3,11 +3,17 @@ package org.mitrofan.bookstore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.Range;
 import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -17,11 +23,20 @@ import java.time.Instant;
 
 import static org.springframework.data.domain.Range.Bound.inclusive;
 
-@DataMongoTest
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 @Import({GlobalTestConfiguration.class, StorageConfiguration.class})
-class BookRepositoryTests {
+@Testcontainers
+class BookRepositoryAgainstContainerTests {
     @Autowired
     BookRepository repository;
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.1");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @BeforeEach
     void beforeEach() {
